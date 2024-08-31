50dcf5dc0a4143ac28727ffe75672ecdce30a83c
Isurika Gunasekara <isurikauma@gmail.com>
2024-08-31 12:17:03 +0530

    sdf

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
 import '../styles/styles.css';
 import { Link } from 'react-router-dom';
 
