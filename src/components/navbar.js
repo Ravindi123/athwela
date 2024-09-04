9c244aa2391019803ecf0387b31ee4334d9dd064
Ravindi Gunarathna <36957677+Ravindi123@users.noreply.github.com>
2024-09-04 23:56:56 +0530

    Refactor navbar component to remove commented out code and unused imports

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,37 +1,4 @@
-<<<<<<< HEAD
-// import React from 'react';
-// import styles from '../styles/styles.module.css'
-// import { Link } from 'react-router-dom';
-
-// const Navbar = () => {
-//     return (
-//         <>
-//             <a href="#" class="logo">
-//             <img src="/logo.jpg" alt=""/>
-//         </a>
-//         <i class='bx bx-menu' id="menu-icon"></i>
-//         <ul class="navbar">
-//             <li><Link to="home">Home</Link></li>
-//             <li><a href="/home#about">About us</a></li>
-//             <li><a href="#projects">Projects</a></li>
-//             <li><Link to="select">Sign Up</Link></li>
-//             <li><Link to="login">Log In</Link></li>
-//         </ul>
-//         <div class="search-container">
-//             <input type="text" id="search-input" placeholder="Search.."/>
-//             <button id="search-btn">
-//                 <i class='bx bx-search-alt' id="search-icon"></i>
-//             </button>
-//         </div>
-//         </>
-//     );
-// };
-
-// export default Navbar;
-import React, { useEffect, useState, useRef } from 'react';
-=======
 import React from 'react';
->>>>>>> 56eb5dbb217d304fcd48350140dbae5f26c3afe9
 import styles from '../styles/styles.module.css';
 import { Link } from 'react-router-dom';
 
