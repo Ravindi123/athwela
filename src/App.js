eb8f136b20098c3c5c3ca04feef3ac0bc678b6b2
desilvaaddt <dinarathis@yahoo.com>
2024-09-06 16:36:22 +0530

    Help Page is added ;)

--- src/App.js
+++ src/App.js
@@ -16,11 +16,14 @@ import SelectProject from './pages/selectProject';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import UserProfile from './pages/userProfile';
-import {auth} from './firebase';
-import {useState, useEffect} from 'react';
+import { auth } from './firebase';
+import { useState, useEffect } from 'react';
+
+import HelpPage from './pages/helpPage';  // Ensure the correct path
+
 
 function App() {
-  const [user , setUser] = useState();
+  const [user, setUser] = useState();
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
       setUser(user);
@@ -30,28 +33,30 @@ function App() {
     <FirebaseProvider>
       <div className="App">
         <Router>
-      <header><Navbar/></header>
+          <header><Navbar /></header>
 
-      <Routes>
-        
-        <Route path="/" element={<>
-          <div><Home/></div>
-          <footer><Footer/></footer></>} />
-          <Route path="/home" element={<>
-          <div><Home/></div>
-          <footer><Footer/></footer></>} />
+          <Routes>
+
+            <Route path="/" element={<>
+              <div><Home /></div>
+              <footer><Footer /></footer></>} />
+            <Route path="/home" element={<>
+              <div><Home /></div>
+              <footer><Footer /></footer></>} />
             <Route path="/select" element={
-          <div><Select/></div>}/>
+              <div><Select /></div>} />
             <Route path="/healthcare" element={<>
-          <div><Healthcare/></div>
-          <footer><Footer/></footer></>} />
-        <Route path="/login" element={<div><Login/></div>} />
-        <Route path="/signup" element={<div><Signup/></div>} />
-        <Route path="/signupOrganization" element={<div><SignupOrganization/></div>} />
-        <Route path="/selectProject" element={<div><SelectProject/></div>}/>
-        <Route path="/projectRegistration" element={<div><ProjectRegistration/></div>}/>
-        <Route path="/homeRegistration" element={<div><HomeRegistration/></div>}/>
-        <Route path="/userProfile" element={<div><UserProfile/></div>}/>
+              <div><Healthcare /></div>
+              <footer><Footer /></footer></>} />
+            <Route path="/login" element={<div><Login /></div>} />
+            <Route path="/signup" element={<div><Signup /></div>} />
+            <Route path="/signupOrganization" element={<div><SignupOrganization /></div>} />
+            <Route path="/selectProject" element={<div><SelectProject /></div>} />
+            <Route path="/projectRegistration" element={<div><ProjectRegistration /></div>} />
+            <Route path="/homeRegistration" element={<div><HomeRegistration /></div>} />
+            <Route path="/userProfile" element={<div><UserProfile /></div>} />
+            
+            <Route path="/helpPage" element={<div><HelpPage/></div>}/>
           </Routes>
           <ToastContainer />
 
